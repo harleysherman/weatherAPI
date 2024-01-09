058654d89e39bc7bf7c74fe941acf8e3203125b0
harleysherman <harsatr11@icloud.com>
2024-01-08 22:22:58 -0600

    add starter code

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function searchWeatherAPI() {
+    var weatherUrl = 'https://www.loc.gov/search/?fo=jsohttps://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={b06c4b04aa915b047a4528bccb03cad5}';
+
+    
+
+}
