bbc0eebd2251af6f959be2ea62176b6a7803150d
harleysherman <harsatr11@icloud.com>
2024-01-10 10:20:54 -0600

    add javaascript to display city and temp

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,26 +2,47 @@ var citySearchHTML = document.querySelector('citySearch');
 var searchButtonHTML = document.querySelector('#searchButton');
 var userCityHTML = document.querySelector('#userCity');
 var cityNameHTML = document.querySelector('#cityName');
+var cityTempHTML = document.querySelector('#temp');
+var cityWindHTML = document.querySelector('#wind');
+var cityHumidityHTML = document.querySelector('#humidity');
 var fiveDayForecastHTML = document.querySelector('#fiveDayForecast');
 var apiKey = "b06c4b04aa915b047a4528bccb03cad5";
 
+function displayUserCity (cityResults) {
+  var userCity = cityResults.name;
+  cityNameHTML.textContent = userCity;
+}
+
 function weatherAPI(cityName) {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
 
     fetch(weatherUrl)
     .then(function (response) {
       if (!response.ok) {
         throw response.json();
       }
-
       return response.json();
     })
     .then(function (cityResults) {
       // write query to page so user knows what they are viewing
       //fiveDayForecastHTML.textContent = fiveDay.search.query;
       console.log(cityResults);
+      //play with city results
+      var userCityTemp = cityResults.main.temp;
+      cityTempHTML.textContent = "Temperature: " + userCityTemp + "\u00B0 F";  
+    
+      var userCityWind = cityResults.wind.speed;
+      cityWindHTML.textContent = "Wind: " + userCityWind + " MPH";
+    
+      var userCityHumidity = cityResults.main.humidity;
+      cityHumidityHTML.textContent = "Humidity: " + userCityHumidity + " %";
+
+      //call to display user's city entered
+      displayUserCity(cityResults);
+
       var lat = cityResults.coord.lat;
       var lon = cityResults.coord.lon;
+
       fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey) 
       .then (function(response2) {
         if (!response2.ok) {
@@ -31,6 +52,17 @@ function weatherAPI(cityName) {
       })
       .then(function(fiveDay) {
         console.log(fiveDay);
+        //array of 40 for loop
+        // for (let i = 0; i <= cityResults.length; i+8) {
+        //   var userCityTemp = cityResults.main.temp;
+        //   cityTempHTML.textContent = "Temperature: " + userCityTemp + "\u00B0 F";  
+        
+        //   var userCityWind = cityResults.wind.speed;
+        //   cityWindHTML.textContent = "Wind: " + userCityWind + " MPH";
+        
+        //   var userCityHumidity = cityResults.main.humidity;
+        //   cityHumidityHTML.textContent = "Humidity: " + userCityHumidity + " %";
+        // }
       })
     })
 }
@@ -47,10 +79,3 @@ function handleSearchFormSubmit(event) {
   }
   
   searchButtonHTML.addEventListener("click", handleSearchFormSubmit);
-
-  function displayUserCity (response2) {
-    var userCity = response2.coord.name;
-    cityNameHTML.textContent = userCity;
-  }
-
-  displayUserCity(response2);
